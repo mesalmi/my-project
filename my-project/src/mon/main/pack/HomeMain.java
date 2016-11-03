2797131bb07324027203ec70cef302e35db14d72
ZM2B9DAN <ziad-externe.mesalmi@edf.fr>
2016-11-03 16:16:58 +0100

    Deuxième commit

--- my-project/src/mon/main/pack/HomeMain.java
+++ my-project/src/mon/main/pack/HomeMain.java
@@ -3,6 +3,7 @@ package mon.main.pack;
 public class HomeMain {
   public static void main(final String args[]) {
     System.out.println("Hello World");
+    System.out.println("TEST commit");
   }
 
 }
